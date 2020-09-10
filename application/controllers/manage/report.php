a02a6e0f954bbcddd71dbf4edb493aaa8c9c77a2
Nattanai <614259013@webmail.npru.ac.th>
2020-09-10 16:21:04 +0700

    Codeigniter Session 1

---
+++ application/controllers/manage/report.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Report extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+    public function __construct()
+	{
+        parent::__construct();
+		echo "Constructor"."</br>" ;
+	}
+	public function index()
+	{
+		echo "Report Index" ;
+    }
+    public function _hello()
+	{
+		echo "</br>"."Hello word" ;
+    }
+    public function showname($name="john",$lastname="wick")
+	{
+        echo "My name is".$name." ".$lastname ;
+        $this->_hello();
+	}
+	public function showview()
+	{
+		$this->load->view("hello_view") ;
+		$this->load->view("hello_view") ;
+		$this->load->view("hello_view") ;
+	}
+	public function showuser()
+	{
+		$data['name'] = "Mario";
+		$data['last'] = "Maoloer" ;
+		$data['age'] = 18 ;
+		$this->load->view("user_page",$data);
+	}
+	public function getuser($p1=null,$p2=null,$p3=null)
+	{
+		$data['name'] = $p1;
+		$data['last'] = $p2 ;
+		$data['age'] = $p3 ;
+		$this->load->view("user_page",$data);
+	}
+	public function showbooks(){
+		$data['books'] = array("Java","Kotlin","PHP");
+		$this->load->view("books_page",$data);
+	}
+}
---
+++ application/views/user_page.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+    echo "Username : ".$name."</br>";
+    echo "Lastname : ".$last."</br>";
+    echo "Age : ".$age."</br>";
+?>
+<center><h1><?=$name?></h1></center>
